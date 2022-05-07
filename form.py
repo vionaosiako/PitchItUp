c2817bcad9e3784aede29c732355b17fdbc32362
vionaosiako <viona.osiako@student.morigaschool.com>
2022-05-07 18:59:30 +0300

    Add form.py

---
+++ form.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('Sign Up')
+    
+class LogInForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+    
+class CommentForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    comment = StringField('Comment', validators=[DataRequired()])
+    submit = SubmitField('Comment')
+    
+class AddPitchForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    newPitch =StringField('New Pitch', validators=[DataRequired()])
+    submit = SubmitField('Submit Pitch')
+    
